96c60177284702d57c37d62a22964870e3c2ddf9
raeyoung <raeyoung233@gmail.com>
2022-05-26 16:51:38 +0900

    First commit

---
+++ src/main/java/com/map/MyTripMap/MyTripMapApplication.java
@@ -0,0 +1,13 @@
+package com.map.MyTripMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTripMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTripMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/MyTripMap/MyTripMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.MyTripMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
